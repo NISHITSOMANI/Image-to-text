f7a2ba4009c8b62c70c96f79330295b6d83c4b3e
NISHITSOMANI <167987929+NISHITSOMANI@users.noreply.github.com>
2025-04-19 20:26:26 +0530

    Update index.py

--- index.py
+++ index.py
@@ -30,6 +30,7 @@ def home():
             img = preprocess_image(img)
             extracted_text = pytesseract.image_to_string(img)
     return render_template("index.html", text=extracted_text)
-
+    
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
+
